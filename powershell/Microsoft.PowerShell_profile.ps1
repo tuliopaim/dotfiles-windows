5c62b6ced57fbccf8bb9dbb75bc0821fd5c691ac
Túlio Paim <tutpaim@gmail.com>
2022-10-08 12:49:15 -0300

    fix powershell profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,8 @@
 oh-my-posh init pwsh --config C:\Users\tutpa\AppData\Local\Programs\oh-my-posh\themes/bubbles.omp.json | Invoke-Expression
 
-Import-Module PSFzf
-
 # PSFzf Config
 
 Import-Module PSFzf
-
-# replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
 
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
