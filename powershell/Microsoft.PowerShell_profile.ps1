e0f04b2f63420e733e82a18ff7dcb89e4a89f8f1
Túlio Paim <tutpaim@gmail.com>
2022-10-11 12:17:38 -0300

    new nvim structure and configs + terminal tweaks

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -12,4 +12,20 @@ function ff{
    nvim $(fzf)
 }
 
+# Aliases
+
+Function ChangeTo([string] $path) { Set-Location -Path $path }
+
+Function ChangeToDev{ChangeTo('C:\dev\')}
+Function ChangeToDotFiles{ChangeTo('C:\Users\tutpa\.dotfiles\')}
+Function ChangeToParent{ChangeTo('..')}
+Function ChangeToParent2{ChangeTo('..\..')}
+Function ChangeToParent3{ChangeTo('..\..\..')}
+
+Set-Alias -Name dev -Value ChangeToDev
+Set-Alias -Name dotfiles -Value ChangeToDotFiles
+Set-Alias -Name .. -Value ChangeToParent
+Set-Alias -Name .... -Value ChangeToParent2
+Set-Alias -Name ...... -Value ChangeToParent3
+
 Clear
