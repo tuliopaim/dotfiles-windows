0545f32bf29dc7ecc17880885be2005899044f29
Túlio Paim <tutpaim@gmail.com>
2022-12-09 20:04:26 -0300

    update vim config

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -25,6 +25,9 @@ keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
+keymap('n', "<C-d>", "<C-d>zz", opts)
+keymap('n', "<C-u>", "<C-u>zz", opts)
+
 -- Resize with arrows
 keymap("n", "<C-Up>", ":resize +2<CR>", opts)
 keymap("n", "<C-Down>", ":resize -2<CR>", opts)
@@ -34,6 +37,16 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Find files
+keymap("n", "<C-t>", "<cmd>Telescope find_files<cr>", opts)
+keymap("n", "<c-f>", "<cmd>Telescope live_grep<cr>", opts)
+keymap("n", "<c-f>", "<cmd>Telescope git_files<cr>", opts)
+
+-- Nvimtree
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
+keymap("n", "<leader>.", ":NvimTreeFocus<cr>", opts)
+
 
 -- Insert --
 -- Press jk fast to enter
@@ -56,11 +69,3 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
---Find files
-keymap("n", "<C-t>", "<cmd>Telescope find_files<cr>", opts)
-keymap("n", "<c-f>", "<cmd>Telescope live_grep<cr>", opts)
-keymap("n", "<c-f>", "<cmd>Telescope git_files<cr>", opts)
---keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
-
--- Nvimtree
-keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
