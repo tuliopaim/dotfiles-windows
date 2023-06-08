6a833cd289dc7391d3f14e38ed12995d4565445a
Túlio Paim <tutpaim@gmail.com>
2023-06-08 12:13:28 -0300

    update remap.lua

--- nvim/lua/tuliopaim/remap.lua
+++ nvim/lua/tuliopaim/remap.lua
@@ -15,7 +15,7 @@ vim.api.nvim_set_keymap('n', "<C-u>", "<C-u>zz", opts)
 -- Move to previous/next
 vim.api.nvim_set_keymap('n', 'gj', ':bprev<enter>', opts)
 vim.api.nvim_set_keymap('n', 'gk', ':bnext<enter>', opts)
-vim.api.nvim_set_keymap('n', '<leader>x', ':bdelete<CR>', opts)
+vim.api.nvim_set_keymap('n', '<leader>x', ':BufferDelete<CR>', opts)
 vim.api.nvim_set_keymap('n', 'gpt', ':BufferPin<CR>', opts)
 
 vim.api.nvim_set_keymap("n", "<C-Up>", ":resize +2<CR>", opts)
